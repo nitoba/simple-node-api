511fc60669460c51ac97a35cb8c3e73a2bc3a410
Bruno Alves <bruno@sthorm.io>
2023-02-08 01:02:00 -0300

    feat: change node version

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,16 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
-const connection =
-  env.DATABASE_CLIENT === 'pg'
-    ? env.DATABASE_URL
-    : {
-        filename: env.DATABASE_URL,
-      }
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection,
+  connection:
+    env.DATABASE_CLIENT === 'pg'
+      ? env.DATABASE_URL
+      : {
+          filename: env.DATABASE_URL,
+        },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
